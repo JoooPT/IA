e0176153d30bb3863710120996da169cc0e658a6
João Ribeiro <joao.viegas.ribeiro@tecnico.ulisboa.pt>
2024-04-26 15:26:27 +0100

    mewing

--- src/pipe.py
+++ src/pipe.py
@@ -1,7 +1,43 @@
-from sys import stdin
+# pipe.py: Template para implementação do projeto de Inteligência Artificial 2023/2024.
+# Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
+# Além das funções e classes sugeridas, podem acrescentar outras que considerem pertinentes.
+
+# Grupo 00:
+# 00000 Nome1
+# 00000 Nome2
+
+import sys 
+from search import (
+    Problem,
+    Node,
+    astar_search,
+    breadth_first_tree_search,
+    depth_first_tree_search,
+    greedy_search,
+    recursive_best_first_search,
+)
+
+
+class PipeManiaState:
+    state_id = 0
+
+    def __init__(self, board):
+        self.board = board
+        self.id = PipeManiaState.state_id
+        PipeManiaState.state_id += 1
+
+    def __lt__(self, other):
+        return self.id < other.id
+
+    # TODO: outros metodos da classe
+
 
 class Board:
-    """ Representação interna de uma grelha de PipeMania. """
+    """Representação interna de um tabuleiro de PipeMania."""
+
+    def get_value(self, row: int, col: int):
+        """Devolve o valor na respetiva posição do tabuleiro."""
+        return self.matrix[row][col]
 
     def __init__(self):
         self.matrix = []
@@ -41,11 +77,11 @@ class Board:
 
     # TODO: outros metodos da classe
 
-    def print_board(self):
+    def print(self):
         print(self.matrix)
 
     @staticmethod
-    def parse_instance(board):
+    def parse_instance():
         """Lê a instância do problema do standard input (stdin)
         e retorna uma instância da classe Board.
         Por exemplo:
@@ -53,12 +89,58 @@ class Board:
         > from sys import stdin
         > line = stdin.readline().split()
         """
-
-        line = stdin.readline().split()
+        board = Board()
+        line = sys.stdin.readline().split()
         size = line.__len__()
         board.add_line(line)
 
         for i in range(size-1):
-            line = stdin.readline().split()
+            line = sys.stdin.readline().split()
             board.add_line(line)
-        return
+        return board
+
+
+class PipeMania(Problem):
+    def __init__(self, board: Board):
+        """O construtor especifica o estado inicial."""
+        # TODO
+        pass
+
+    def actions(self, state: PipeManiaState):
+        """Retorna uma lista de ações que podem ser executadas a
+        partir do estado passado como argumento."""
+        # TODO
+        pass
+
+    def result(self, state: PipeManiaState, action):
+        """Retorna o estado resultante de executar a 'action' sobre
+        'state' passado como argumento. A ação a executar deve ser uma
+        das presentes na lista obtida pela execução de
+        self.actions(state)."""
+        # TODO
+        pass
+
+    def goal_test(self, state: PipeManiaState):
+        """Retorna True se e só se o estado passado como argumento é
+        um estado objetivo. Deve verificar se todas as posições do tabuleiro
+        estão preenchidas de acordo com as regras do problema."""
+        # TODO
+        pass
+
+    def h(self, node: Node):
+        """Função heuristica utilizada para a procura A*."""
+        # TODO
+        pass
+
+    # TODO: outros metodos da classe
+
+
+if __name__ == "__main__":
+    # TODO:
+    # Ler o ficheiro do standard input
+    board = Board.parse_instance()
+    board.print()
+    # Usar uma técnica de procura para resolver a instância,
+    # Retirar a solução a partir do nó resultante,
+    # Imprimir para o standard output no formato indicado.
+    pass
